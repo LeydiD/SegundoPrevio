48eff37eb1f121034301143065355302ef83ba85
LeydiD <leydialejaduran@gmail.com>
2024-11-08 12:11:12 -0500

    completar servicios

--- src/main/java/co/edu/ufps/services/DepartmentService.java
+++ src/main/java/co/edu/ufps/services/DepartmentService.java
@@ -1,5 +1,50 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Department;
+import co.edu.ufps.repositories.DepartmentRepository;
+
+@Service
 public class DepartmentService {
 
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	public List<Department> list() {
+		return departmentRepository.findAll();
+	}
+
+	public Department create(Department department) {
+		return departmentRepository.save(department);
+	}
+
+	public Optional<Department> update(Integer id, Department departmentDetails) {
+		Optional<Department> optionaldepartment = departmentRepository.findById(id);
+		if (!optionaldepartment.isPresent()) {
+			return Optional.empty();
+		}
+
+		Department department = optionaldepartment.get();
+		department.setName(departmentDetails.getName());
+		return Optional.of(departmentRepository.save(department));
+	}
+
+	public Optional<Department> getById(Integer id) {
+		return departmentRepository.findById(id);
+	}
+
+	public boolean delete(Integer id) {
+		if (!departmentRepository.existsById(id)) {
+			return false;
+		}
+
+		departmentRepository.deleteById(id);
+		return true;
+	}
 }
--- src/main/java/co/edu/ufps/services/RoleService.java
+++ src/main/java/co/edu/ufps/services/RoleService.java
@@ -1,5 +1,50 @@
 package co.edu.ufps.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.ufps.entities.Role;
+import co.edu.ufps.repositories.RoleRepository;
+
+@Service
 public class RoleService {
 
+	@Autowired
+	RoleRepository roleRepository;
+
+	public List<Role> list() {
+		return roleRepository.findAll();
+	}
+
+	public Role create(Role role) {
+		return roleRepository.save(role);
+	}
+
+	public Optional<Role> update(Integer id, Role roleDetails) {
+		Optional<Role> optionalrole = roleRepository.findById(id);
+		if (!optionalrole.isPresent()) {
+			return Optional.empty();
+		}
+
+		Role role = optionalrole.get();
+		role.setName(roleDetails.getName());
+		return Optional.of(roleRepository.save(role));
+	}
+
+	public Optional<Role> getById(Integer id) {
+		return roleRepository.findById(id);
+	}
+
+	public boolean delete(Integer id) {
+		if (!roleRepository.existsById(id)) {
+			return false;
+		}
+
+		roleRepository.deleteById(id);
+		return true;
+	}
 }
